6cfd67715ec8a016d3459f7f73315aefcc4d64ee
tuan <rickytuan1991@gmail.com>
2023-04-17 19:04:10 +0300

    update keyboard event

--- front-end/src/components/Key/Key.tsx
+++ front-end/src/components/Key/Key.tsx
@@ -3,11 +3,23 @@ import { useAppDispatch, useAppSelector } from "../../hooks/hooks";
 import { addGuessedLetter } from "../../features/GameSlice";
 
 import classes from "./key.module.css";
+import { useEffect } from "react";
 
 const Key: React.FC<KeyProps> = ({ letter, status }) => {
   const dispatch = useAppDispatch();
   const gameStatus = useAppSelector((state) => state.game.gameStatus);
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === letter.toLowerCase() && gameStatus === "playing" && status === "blank") {
+        dispatch(addGuessedLetter(letter));
+      }
+    };
+    window.addEventListener("keydown", handleKeyDown);
 
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [letter, status, gameStatus, dispatch]);
   return (
     <div className={classes.key_container}>
       <button
