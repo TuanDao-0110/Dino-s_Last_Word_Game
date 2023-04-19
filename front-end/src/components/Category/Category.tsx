c6bd67ec132f5f13ba6e0b1e796ffa0a0f7d510a
Vagapova Valeriia <lera.vagapova@gmail.com>
2023-04-20 01:47:32 +0300

    move hint button, style paddings on category panel

--- front-end/src/components/Category/Category.tsx
+++ front-end/src/components/Category/Category.tsx
@@ -3,14 +3,25 @@ import React, { ReactEventHandler } from "react";
 
 // Redux
 import { useAppSelector, useAppDispatch } from "../../hooks/hooks";
-import { getWordDispatch, setCategory, setWordToGuess } from "../../features/GameSlice";
+import {
+  getWordDispatch,
+  setCategory,
+  setWordToGuess,
+} from "../../features/GameSlice";
 
 // Bootstrap
 import Form from "react-bootstrap/Form";
 
 // Components
-import { Message, Controls, PopoverHint } from "../../assets/export_component/resource";
-import { CircularProgressbarWithChildren, buildStyles } from "react-circular-progressbar";
+import {
+  Message,
+  Controls,
+  PopoverHint,
+} from "../../assets/export_component/resource";
+import {
+  CircularProgressbarWithChildren,
+  buildStyles,
+} from "react-circular-progressbar";
 
 // Types
 import { Categories, GameStatus } from "../../types/API.model";
@@ -19,7 +30,8 @@ import { Categories, GameStatus } from "../../types/API.model";
 import classes from "./category.module.css";
 
 const Category: React.FC = () => {
-  const { category, score, round, gameStatus, guessedLetters, wordToGuess } = useAppSelector((state) => state.game);
+  const { category, score, round, gameStatus, guessedLetters, wordToGuess } =
+    useAppSelector((state) => state.game);
   const dispatch = useAppDispatch();
 
   const renderOption = () => {
@@ -36,17 +48,24 @@ const Category: React.FC = () => {
   };
 
   const getMeteorProgress = () => {
-    const wrongGuesses = guessedLetters.filter((letter) => !wordToGuess.includes(letter)).length;
+    const wrongGuesses = guessedLetters.filter(
+      (letter) => !wordToGuess.includes(letter)
+    ).length;
     return (wrongGuesses / 9) * 100;
   };
 
   const getMeteorClass = () => {
-    const wrongGuesses = guessedLetters.filter((letter) => !wordToGuess.includes(letter)).length;
+    const wrongGuesses = guessedLetters.filter(
+      (letter) => !wordToGuess.includes(letter)
+    ).length;
     return Math.floor(wrongGuesses / 3);
   };
 
   return (
     <div className={classes.category_container}>
+      <div className={classes.popover_container}>
+        <PopoverHint />
+      </div>
       <div className={classes.category_section}>
         <div className={classes.game_status_container}>
           <p className={classes.score}>
@@ -70,9 +89,19 @@ const Category: React.FC = () => {
               pathTransitionDuration: 0.5,
             })}
           >
-            <p className={`${classes.tries} ${classes["wrong" + getMeteorClass()]}`}>
+            <p
+              className={`${classes.tries} ${
+                classes["wrong" + getMeteorClass()]
+              }`}
+            >
               <span>
-                <span>{guessedLetters.filter((letter) => !wordToGuess.includes(letter)).length}</span>
+                <span>
+                  {
+                    guessedLetters.filter(
+                      (letter) => !wordToGuess.includes(letter)
+                    ).length
+                  }
+                </span>
                 /9
               </span>{" "}
               tries
@@ -81,23 +110,30 @@ const Category: React.FC = () => {
         </div>
       </div>
       <div className={classes.category_info}>
-        <p className={classes.category_display}>Word category: {/* <span>{category}</span> */}</p>{" "}
+        <p className={classes.category_display}>
+          Word category: {/* <span>{category}</span> */}
+        </p>{" "}
         <Form.Select
-          disabled={guessedLetters.length > 0 || gameStatus !== GameStatus.playing}
+          disabled={
+            guessedLetters.length > 0 || gameStatus !== GameStatus.playing
+          }
           aria-label="select category"
           onChange={setUpCategory}
           className={classes.category_select}
         >
           {renderOption()}
         </Form.Select>
-        <p className={classes.points_earned}>Points to be earned: {category === "all" ? 2 : 1}</p>
+        <p className={classes.points_earned}>
+          Points to be earned: {category === "all" ? 2 : 1}
+        </p>
       </div>
       <div className={classes.category_section}>
-        <div className={classes.popover_container}>
-          <PopoverHint />
-        </div>
-        {gameStatus === GameStatus.win && <p className={classes.game_status}>You won!</p>}
-        {gameStatus === GameStatus.lose && <p className={classes.game_status}>Ouch. You lost!</p>}
+        {gameStatus === GameStatus.win && (
+          <p className={classes.game_status}>You won!</p>
+        )}
+        {gameStatus === GameStatus.lose && (
+          <p className={classes.game_status}>Ouch. You lost!</p>
+        )}
 
         <Controls />
       </div>
